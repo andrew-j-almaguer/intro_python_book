12fa36abdfe60690cefe6aefc8cac7b3d9f7d9e8
Andrew Almaguer <andrew.almaguer@protonmail.com>
2024-10-29 19:37:12 +0000

    Chapter 7: Introduction to Collections

---
+++ intro_to_collections/exercise_10.py
@@ -0,0 +1,9 @@
+# Bob expects the following code to print the names in alphabetical order.
+# Without running the code, can you say whether it will? Explain your answer.
+names = { 'Chris', 'Clare', 'Karis', 'Karl',
+          'Max', 'Nick', 'Victor' }
+print(names)
+
+# It may, but probably will not, print the names in alphabetical order because
+# Bob has entered the names in a set, which is an unordered collection type.
+# Insertion order does not matter in unordered collections.
